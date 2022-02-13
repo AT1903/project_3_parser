0500f5f40ec3ea2a2443ded73b37618ce6250533
AT1903 <tihonov1903@gmail.com>
2022-02-13 22:47:41 +0500

    async_fun
    парсинг с главной страницы и страницы 2 уровняю. начало работы по асинхронному парсингу

--- parser_2.py
+++ parser_2.py
@@ -26,7 +26,14 @@ import time
 import datetime
 
 start_time = time.time()#время начала выполнения скрипта
+refs_homepage = [] #создаем список для хранения ссылок с главной страницы
+refs_page2 = [] #создаем список для хранения ссылок со страницы 2-го уровня
 
+res = dict() #словарь со сслыками
+headers = {
+            'accept' : 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+            'user-agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.82 Safari/537.36'
+        }  
 
 print('--------------------------')
 print('-----start parser---------')
@@ -34,19 +41,33 @@ print('--------------------------')
 
 #Функция парсинга ссылок с главной страницы
 def fun_parser_homepage():
-    headers = {
-            'accept' : 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-            'user-agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.82 Safari/537.36'
-        }
     url = 'https://chastotnik.shop/'
     response = requests.get(url=url, headers=headers)
     soup = BeautifulSoup(response.text, 'lxml')  #lxml это быстрая и гибкая библиотека для обработки разметки XML и HTML на Python    
-    refs_homepage = set()                        #создаем пустое множество для хранения ссылок с главной страницы
     refs = soup.find_all('a', class_= "show-more") #поиск все элементов с кнопок "смотреть все"
-    for link in refs: refs_homepage.add(link.get('href')) #копируем в множество всей найденный ссылки
-    print (*refs_homepage, sep = "\n") # печать результатов
-    
+    for link in refs: refs_homepage.append(link.get('href')) #копируем в список все найденные ссылки
+    #print (*refs_homepage, sep = "\n") # печать результатов
 
+#Функция парсинга ссылок со страницы 2-го уровня
+def fun_parser_2page(url):      
+    response = requests.get(url=url, headers=headers)
+    soup = BeautifulSoup(response.text, 'lxml')  #lxml это быстрая и гибкая библиотека для обработки разметки XML и HTML на Python
+    refs = soup.find('div', class_= "card card-subcategory").find_all('a') #поиск всех элементов типа "а" со страницы 2    
+    for link in refs:        
+        refs_page2.append(link.get('href')) #копируем в список все найденный ссылки
+    #print (*refs_page2, sep = "\n") # печать результатов
+
+#Функция парсинга ссылок со страницы 2-го уровня
+async def fun_asy_parser_2page(session, url):
+    async with session.get(url=url, headers=headers) as response:
+        #response_text = await response.text()      
+        #response = await requests.get(url=url, headers=headers)
+        response_text = await response.text()
+        soup = BeautifulSoup(await response_text, 'lxml')  #lxml это быстрая и гибкая библиотека для обработки разметки XML и HTML на Python
+        refs = soup.find('div', class_= "card card-subcategory").find_all('a') #поиск всех элементов типа "а" со страницы 2    
+        for link in refs:        
+            refs_page2.append(link.get('href')) #копируем в список все найденный ссылки
+        #print (*refs_page2, sep = "\n") # печать результатов
 
 #создаем асинхронную функция для парсинга  
 async def fun_parser():
@@ -59,16 +80,25 @@ async def fun_parser():
 #создаем асинхронную функция для формирование списка задач  
 async def fun_creater_tasks():  
     #Устанавливаем сессию ClientSession из aiohttp.
-    async with aiohttp.ClientSession() as sesion:
-        #создаем пустой список tasks
+    async with aiohttp.ClientSession() as session:
+        #создаем пустой список tasks        
         tasks = []
+        for page in refs_homepage:
+            task = asyncio.create_task(fun_asy_parser_2page(session, page))
+            tasks.append(task)
 
-        url = 'https://chastotnik.shop/preobrazovateli-chastoti'
-        headers = {
-            'accept' : 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-            'user-agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.82 Safari/537.36'
-        }
-        response = requests.get(url=url, headers=headers)   
+        await asyncio.gather(*tasks)  
+
+async def fun_creater_tasks2():  
+    #Устанавливаем сессию ClientSession из aiohttp.
+    async with aiohttp.ClientSession() as session:
+        #создаем пустой список tasks        
+        tasks = []
+        for page in refs_homepage:
+            task = asyncio.create_task(fun_asy_parser_2page(session, page))
+            tasks.append(task)
+
+        await asyncio.gather(*tasks)  
 
 def main():
     print('main')
@@ -76,6 +106,14 @@ def main():
     print (f'текущая дата {curr_date}')
 
     fun_parser_homepage()
+
+    #for r in refs_homepage:
+    #    fun_parser_2page(r)#refs_homepage[0])
+    #print (*refs_page2, sep = "\n") # печать результатов
+
+    #запуск асинхронного парсинга
+    asyncio.run(fun_creater_tasks())
+    print (*refs_page2, sep = "\n") # печать результатов
 
     finish_time = time.time() - start_time #общее время выполнения скрипта
     print(f'Затраченное время: {finish_time}')
