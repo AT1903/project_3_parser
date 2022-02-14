9cf5ceee1a30c99491cbedc0b94ef83976358913
AT1903 <tihonov1903@gmail.com>
2022-02-14 22:14:00 +0500

    parser_2_async
    solved "RuntimeError: Event loop is closed"
    create parser_2_async

--- parser_2_async.py
+++ parser_2_async.py
@@ -29,7 +29,6 @@ start_time = time.time()#время начала выполнения скрип
 refs_homepage = [] #создаем список для хранения ссылок с главной страницы
 refs_page2 = [] #создаем список для хранения ссылок со страницы 2-го уровня
 
-res = dict() #словарь со сслыками
 headers = {
             'accept' : 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
             'user-agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.82 Safari/537.36'
@@ -48,15 +47,6 @@ def fun_parser_homepage():
     for link in refs: refs_homepage.append(link.get('href')) #копируем в список все найденные ссылки
     #print (*refs_homepage, sep = "\n") # печать результатов
 
-#Функция парсинга ссылок со страницы 2-го уровня
-def fun_parser_2page(url):      
-    response = requests.get(url=url, headers=headers)
-    soup = BeautifulSoup(response.text, 'lxml')  #lxml это быстрая и гибкая библиотека для обработки разметки XML и HTML на Python
-    refs = soup.find('div', class_= "card card-subcategory").find_all('a') #поиск всех элементов типа "а" со страницы 2    
-    for link in refs:        
-        refs_page2.append(link.get('href')) #копируем в список все найденный ссылки
-    #print (*refs_page2, sep = "\n") # печать результатов
-
 #Асинхронная функция парсинга ссылок со страницы 2-го уровня
 async def fun_asy_parser_2page(session, url,num):
     async with session.get(url=url, headers=headers) as response:
@@ -70,8 +60,8 @@ async def fun_asy_parser_2page(session, url,num):
         print(f'обработал {num} из {len(refs_homepage)} результатов')
         #print (*refs_page2, sep = "\n") # печать результатов
 
-#создаем асинхронную функция для формирование списка задач  
-async def fun_creater_tasks():  
+#создаем асинхронную функция для формирование списка задач (страницы 2го уровня)
+async def fun_creater_tasks_2():  
     #Устанавливаем сессию ClientSession из aiohttp.
     async with aiohttp.ClientSession() as session:
         #создаем пустой список tasks        
@@ -81,45 +71,25 @@ async def fun_creater_tasks():
             task = asyncio.create_task(fun_asy_parser_2page(session, page,c))
             tasks.append(task)
             c+=1
-
-        await asyncio.gather(*tasks)  
-
-async def fun_creater_tasks2():  
-    #Устанавливаем сессию ClientSession из aiohttp.
-    async with aiohttp.ClientSession() as session:
-        #создаем пустой список tasks        
-        tasks = []
-        for page in refs_homepage:
-            task = asyncio.create_task(fun_asy_parser_2page(session, page))
-            tasks.append(task)
-
-        await asyncio.gather(*tasks)  
+        await asyncio.gather(*tasks)
 
 def main():
     print('main')
     curr_date = datetime.datetime.now().strftime('%d_%m_%Y_%H_%M')
     print (f'текущая дата {curr_date}')
+    #парсинг ссылок с главной страницы
+    fun_parser_homepage() 
 
-    fun_parser_homepage()
-
-    #запуск последовательного парсинга
-    for r in refs_homepage:
-        fun_parser_2page(r)#refs_homepage[0])
-    #print (*refs_page2, sep = "\n") # печать результатов
-
-    #запуск асинхронного парсинга
-    #asyncio.run(fun_creater_tasks())
-    #loop = asyncio.get_event_loop()
-    #loop.run_until_complete(fun_creater_tasks())
-
-
+    #запуск асинхронного парсинга   
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(fun_creater_tasks_2())
 
     print (*refs_page2, sep = "\n") # печать результатов
 
     finish_time = time.time() - start_time #общее время выполнения скрипта
     print(f'Затраченное время: {finish_time}')
     print('************************************************')
-    print('************************************************')
+    print('*****************FINISH*************************')
     print('************************************************')
 
 
